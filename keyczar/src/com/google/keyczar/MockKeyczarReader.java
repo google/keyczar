e2a1bc7ae0e768f85d169fee61a0b0f57819ac38
arkajit.dey <arkajit.dey@8f3b247a-914b-0410-8f51-05e301831c82>
2008-06-20 16:07:47 +0000

    Cleaning up MockKeyczarReader.
    git-svn-id: https://keyczar.googlecode.com/svn/trunk@93 8f3b247a-914b-0410-8f51-05e301831c82

--- keyczar/src/com/google/keyczar/MockKeyczarReader.java
+++ keyczar/src/com/google/keyczar/MockKeyczarReader.java
@@ -1,35 +1,24 @@
 package com.google.keyczar;
 
-import com.google.gson.annotations.Expose;
 import com.google.keyczar.enums.KeyPurpose;
 import com.google.keyczar.enums.KeyType;
 import com.google.keyczar.exceptions.KeyczarException;
 import com.google.keyczar.interfaces.KeyczarReader;
 import com.google.keyczar.util.Util;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
 public class MockKeyczarReader extends KeyMetadata implements KeyczarReader {
   
   public MockKeyczarReader(String n, KeyPurpose p, KeyType t) {
     super(n,p,t);
   }
-  
-  public KeyVersion getVersion(int versionNumber) {
-    return versionMap.get(versionNumber);
-  }
 
   @Override
   public String getKey(int version) throws KeyczarException {
-    // TODO: implement me!
     throw new KeyczarException("Unimplemented!");
   }
 
   @Override
-  public String getMetadata() throws KeyczarException {
+  public String getMetadata() {
     return Util.gson().toJson(this);
   }
-
 }
