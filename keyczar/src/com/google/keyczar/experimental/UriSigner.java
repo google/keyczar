fd048551dcf6ef5371af55386062a857a36ec9a2
arkajit.dey <arkajit.dey@8f3b247a-914b-0410-8f51-05e301831c82>
2008-06-23 19:29:31 +0000

    Added URI canonicalization, javadocs for UriSigner. Added canonicalization test to UriSignerTest.
    git-svn-id: https://keyczar.googlecode.com/svn/trunk@97 8f3b247a-914b-0410-8f51-05e301831c82

--- keyczar/src/com/google/keyczar/experimental/UriSigner.java
+++ keyczar/src/com/google/keyczar/experimental/UriSigner.java
@@ -25,9 +25,15 @@ import java.net.URISyntaxException;
 import java.util.TreeSet;
 
 /**
+ * Adds a signature parameter, named sig by default, to a URI query which
+ * signs all the query parameters. Can use to check integrity of query
+ * parameters. Canonicalizes URI query parameters to be in lexicographic order.
+ * 
  * Under construction. This class will most likely change.
  * 
  * @author steveweis@gmail.com (Steve Weis)
+ * @author arkajit.dey@gmail.com (Arkajit Dey)
+ * 
  */
 public class UriSigner {
   private static final String DEFAULT_SIG_PARAM = "sig";
@@ -41,13 +47,35 @@ public class UriSigner {
     signer = new Signer(fileLocation);
   }
   
+  /**
+   * Returns a signed URI with the signature in the default parameter 'sig'.
+   * Rest of the URI and query parameters are unchanged. Entire URI is signed.
+   * 
+   * @param uri to be signed
+   * @return signed uri
+   * @throws KeyczarException
+   */
   public URI sign(URI uri) throws KeyczarException { 
     return sign(uri, DEFAULT_SIG_PARAM);
   }
   
+  /**
+   * Returns a signed URI with the signature in a parameter with the specified
+   * name. Rest of the URI and query parameters are unchanged. 
+   * Entire URI is signed.
+   * 
+   * @param uri to be signed
+   * @param sigParam, String name of signature parameter
+   * @return signed uri with signature in given parameter
+   * @throws KeyczarException
+   */
   public URI sign(URI uri, String sigParam) throws KeyczarException {
+    try {
+      uri = canonicalUri(uri); // CHECK: Canonicalized the URI, OK?
+    } catch (URISyntaxException e) {
+      throw new KeyczarException(e);
+    }
     String uriString = uri.toASCIIString();
-    // TODO: Canonicalize the URI
     String sig = signer.sign(uriString);
     String signedQuery = sigParam + "=" + sig;
     String query = uri.getQuery();
@@ -63,13 +91,36 @@ public class UriSigner {
     }
   }
   
+  /**
+   * Verifies that the given URI is properly signed. Assumes signature is
+   * in query parameter named 'sig'.
+   * 
+   * @param signedUri
+   * @return true if signature is valid, false otherwise
+   * @throws KeyczarException
+   */
   public boolean verify(URI signedUri) throws KeyczarException {
     return verify(signedUri, DEFAULT_SIG_PARAM);
   }
   
+  /**
+   * Verifies that the given URI is properly signed. Takes signature from
+   * from the parameter name given.
+   * 
+   * @param signedUri
+   * @param sigParam
+   * @return true if signature is valid, false otherwise
+   * @throws KeyczarException
+   */
   public boolean verify(URI signedUri, String sigParam)
       throws KeyczarException {
+    if (signedUri == null) {
+      return false; // exception instead?
+    }
     String query = signedUri.getQuery();
+    if (query == null) {
+      //TODO: if query==null? vacuously true? or false? exception?
+    }
     String sig = null;
     StringBuffer unsignedQuery = new StringBuffer();
     
@@ -90,21 +141,29 @@ public class UriSigner {
     try {
       URI unsignedUri;
       if (unsignedQuery.length() > 0) {
-        unsignedQuery.deleteCharAt(unsignedQuery.length() - 1);
+        unsignedQuery.deleteCharAt(unsignedQuery.length() - 1); // extra &
         unsignedUri = new URI(signedUri.getScheme(), signedUri.getAuthority(),
-            signedUri.getPath(), unsignedQuery.toString(), signedUri.getFragment());
+            signedUri.getPath(), unsignedQuery.toString(), 
+            signedUri.getFragment());
       } else {
         unsignedUri = new URI(signedUri.getScheme(), signedUri.getAuthority(),
             signedUri.getPath(), null, signedUri.getFragment());
       }
+      unsignedUri = canonicalUri(unsignedUri); // CHECK: use canonical version
       return signer.verify(unsignedUri.toASCIIString(), sig);
     } catch (URISyntaxException e) {
       // Throw an exception?
       return false;
     }
   }
-
   
+  /**
+   * Return canonical version of query string with all query parameters sorted
+   * in lexicographic order. 
+   * 
+   * @param query to canonicalize
+   * @return canonicalized query String
+   */
   private String canonicalQuery(String query) {
     StringBuffer canonicalQuery = new StringBuffer();
 
@@ -121,5 +180,17 @@ public class UriSigner {
       canonicalQuery.deleteCharAt(canonicalQuery.length() - 1);
     }
     return canonicalQuery.toString();
+  }
+
+  /**
+   * Canonicalizes URI by replacing query component with canonicalized query.
+   * 
+   * @param uri to be canonicalized
+   * @return canonicalized uri
+   * @throws URISyntaxException if uri is invalid
+   */
+  private URI canonicalUri(URI uri) throws URISyntaxException {
+    return (uri == null) ? null : new URI(uri.getScheme(), uri.getAuthority(), 
+        uri.getPath(), canonicalQuery(uri.getQuery()), uri.getFragment());
   }
 }
--- keyczar/tests/com/google/security/keyczar/experimental/UriSignerTest.java
+++ keyczar/tests/com/google/security/keyczar/experimental/UriSignerTest.java
@@ -16,31 +16,63 @@
 
 package com.google.security.keyczar.experimental;
 
-import static org.junit.Assert.assertTrue;
-
 import com.google.keyczar.exceptions.KeyczarException;
 import com.google.keyczar.experimental.UriSigner;
+
+import junit.framework.TestCase;
 
 import org.junit.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
+/**
+ * Tests UriSigner utility.
+ *
+ * @author steveweis@gmail.com (Steve Weis)
+ * @author arkajit.dey@gmail.com (Arkajit Dey)
+ */
 
-public class UriSignerTest {
+public class UriSignerTest extends TestCase {
   String[] testUris = { "www.foo.com", "http://www.foo.com/bar#foo",
     "http://foo.com/q?a=1&b=2&c=3", "http://foo.com/q?c=3&b=2&a=1",
     "http://foo.com/q?c=3&b=2&a=1#blah"
   };
+  UriSigner uriSigner;
+  
+  @Override
+  public final void setUp() throws KeyczarException {
+    uriSigner = new UriSigner("./testdata/hmac");
+  }
   
   @Test
-  public final void testSignAndVerifyURI() throws KeyczarException,
+  public final void testSignAndVerifyUri() throws KeyczarException,
       URISyntaxException {
-    UriSigner uriSigner = new UriSigner("./testdata/hmac");
-    
     for (String uri : testUris) {
       URI signedUri = uriSigner.sign(new URI(uri));
       assertTrue(uriSigner.verify(signedUri));
     }
+  }
+  
+  /**
+   * Tests that the signatures on two URIs with the same canonical
+   * representation are indeed the same.
+   * 
+   * @throws KeyczarException
+   * @throws URISyntaxException
+   */
+  @Test
+  public final void testCanonicalUri() throws KeyczarException, 
+      URISyntaxException {
+    // A and B are same URIs with query parameters in different orders
+    URI signedUriA = uriSigner.sign(new URI(testUris[2]));
+    URI signedUriB = uriSigner.sign(new URI(testUris[3]));
+    // the signature parameter should be the same
+    assertEquals(signedUriA.toASCIIString(), signedUriB.toASCIIString());
+  }
+  
+  @Override
+  public final void tearDown() {
+    uriSigner = null;
   }
 }
