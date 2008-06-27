cdeeed89663f0ee5a33c450c393fbd638ec934f3
steveweis <steveweis@8f3b247a-914b-0410-8f51-05e301831c82>
2008-06-27 21:51:09 +0000

    Playing with AES.
    git-svn-id: https://keyczar.googlecode.com/svn/trunk@122 8f3b247a-914b-0410-8f51-05e301831c82

--- keyczar-python/src/keyczar/test.py
+++ keyczar-python/src/keyczar/test.py
@@ -1,5 +1,12 @@
 # for random testing
 from Crypto.Hash import MD5
+from Crypto.Cipher import AES
 hash=MD5.new()
 hash.update('message')
 print hash.hexdigest()
+
+aes = AES.new("0123456789ABCDEF", AES.MODE_CBC, "0000000000000000")
+cipher = aes.encrypt("FoofFoofFoofFoof")
+print cipher
+aes = AES.new("0123456789ABCDEF", AES.MODE_CBC, "0000000000000000")
+print aes.decrypt(cipher)
