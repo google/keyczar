765b3f62f3ffbcf418aee0aa1a39ef8de9fcd68e
arkajit.dey <arkajit.dey@8f3b247a-914b-0410-8f51-05e301831c82>
2008-06-29 01:48:11 +0000

    Python: Added more KeyTypes, KeyType functions.
    git-svn-id: https://keyczar.googlecode.com/svn/trunk@127 8f3b247a-914b-0410-8f51-05e301831c82

--- keyczar-python/src/keyczar/KeyData.py
+++ keyczar-python/src/keyczar/KeyData.py
@@ -3,10 +3,41 @@ class KeyType:
     self.name = name
     self.id = id
     self.sizes = sizes
+    self.size = sizes[0]  #default size
     self.outputSize = outputSize
 
   def __str__(self):
     return self.name
+  
+  def outputSize(self):
+    return self.outputSize
+  
+  def defaultSize(self):
+    return self.sizes[0]
+  
+  def size(self):
+    return self.size
+  
+  def setSize(self, newSize):
+    if newSize in self.sizes:
+      self.size = newSize
+      
+  def resetSize(self):
+    self.size = self.defaultSize()
+
+class KeyTypes:   
+  AES = KeyType("AES", 0, [128, 192, 256], 0)
+  HMAC_SHA1 = KeyType("HMAC-SHA1", 1, [256], 20)
+  DSA_PRIV = KeyType("DSA Private", 2, [1024], 48)
+  DSA_PUB = KeyType("DSA Public", 3, [1024], 48)
+  RSA_PRIV = KeyType("RSA Private", 4, [2048, 1024, 768, 512], 256)
+  RSA_PUB = KeyType("RSA Public", 4, [2048, 1024, 768, 512], 256)
+  types = {AES.id : AES, HMAC_SHA1.id : HMAC_SHA1, DSA_PRIV.id : DSA_PRIV, 
+           DSA_PUB.id : DSA_PUB, RSA_PRIV.id : RSA_PRIV, RSA_PUB.id : RSA_PUB}
+  
+  def getType(value):
+    if types.has_key(value):
+      return types[value]
 
 class Key:
   """Parent class for Keyczar Keys"""
@@ -26,12 +57,6 @@ class Key:
   
   def hash(self):
     return self.hash
-  
- 
-class KeyTypes:   
-  AES = KeyType("AES", 0, [128, 192, 256], 0)
-  HMAC_SHA1 = KeyType("HMAC-SHA1", 1, [256], 20)
-  types = { AES.id : AES, HMAC_SHA1.id : HMAC_SHA1 }
 
 class KeyMetadata:
   def __init__(self, name, purpose, type, versions):
