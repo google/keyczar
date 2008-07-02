d6246c81d4ad76d81dd505f8acf8f197629cb566
steveweis <steveweis@8f3b247a-914b-0410-8f51-05e301831c82>
2008-07-02 23:59:30 +0000

    Forgot EncryptedReader interface
    git-svn-id: https://keyczar.googlecode.com/svn/trunk@149 8f3b247a-914b-0410-8f51-05e301831c82

---
+++ keyczar-java/src/com/google/keyczar/interfaces/EncryptedReader.java
@@ -0,0 +1,12 @@
+  package com.google.keyczar.interfaces;
+
+import com.google.keyczar.Crypter;
+
+/**
+ * Encrypted Readers are able to read encrypted key sets
+ *
+ * @author steveweis@gmail.com (Steve Weis)
+ *
+ */
+public interface EncryptedReader extends KeyczarReader {
+}
