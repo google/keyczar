53be8c2a042cdcf696ad1f991a92ec31bdf2c9c2
steveweis <steveweis@8f3b247a-914b-0410-8f51-05e301831c82>
2009-06-18 00:41:35 +0000

    Fixing setup.py to build proper tar ball
    git-svn-id: https://keyczar.googlecode.com/svn/trunk@421 8f3b247a-914b-0410-8f51-05e301831c82

--- python/setup.py
+++ python/setup.py
@@ -1,14 +1,6 @@
 from distutils.core import setup
 
 import sys
-try:
-    from setuptools import setup
-    kw = {
-        'install_requires': 'pycrypto >= 1.9',
-    }
-except ImportError:
-    from distutils.core import setup
-    kw = {}
 
 setup(name='python-keyczar',
       description='Toolkit for safe and simple cryptography',
@@ -17,5 +9,6 @@ setup(name='python-keyczar',
       url='http://www.keyczar.org/',
       version='0.6b',
       packages=['keyczar'],
-      **kw
+      package_dir={'keyczar': 'src/keyczar'},
+      requires=['pycrypto (>2.0)'],
 )
