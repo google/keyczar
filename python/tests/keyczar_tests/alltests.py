fdf2d2429f1d67359478760764a6bf5b6210f1bd
Shawn Willden <swillden@google.com>
2011-11-30 15:38:46 -0700

    Modified alltests.py so it actually runs the tests.

--- python/tests/keyczar_tests/alltests.py
+++ python/tests/keyczar_tests/alltests.py
@@ -5,9 +5,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #      http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,4 +34,4 @@ def suite():
   return alltests
 
 if __name__ == '__main__':
-    unittest.main(defaultTest='suite')
+  unittest.TextTestRunner().run(suite())
