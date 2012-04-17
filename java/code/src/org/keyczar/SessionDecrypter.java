0ecb416f4949f5b7cdade3c3986db70735994421
Justin Scheiner <jmscheiner@google.com>
2012-04-17 13:18:41 -0400

    Deprecate SessionEncrypter / SessionDecrypter in favor of merging them into SessionCrypter.

--- java/code/src/org/keyczar/SessionDecrypter.java
+++ java/code/src/org/keyczar/SessionDecrypter.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,11 +23,14 @@ import org.keyczar.exceptions.KeyczarException;
  * A SessionDecrypter will be instantiated with session material containing an
  * encrypted symmetric key. That key will be decrypted with the given
  * {@link Crypter} and used to instantiate another {@link Crypter}.
- * 
+ *
+ * This class and {@link SessionEncrypter} have been deprecated in favor of
+ * {@link SessionCrypter}.
+ *
  * @author steveweis@gmail.com (Steve Weis)
  *
  */
-@Experimental
+@Deprecated
 public class SessionDecrypter {
   private final Crypter symmetricCrypter;
 
