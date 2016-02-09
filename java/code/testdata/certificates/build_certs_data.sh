a4d5a4be7539318d5c203a9922c63fa66830d649
Shawn Willden <swillden@google.com>
2016-02-09 14:18:37 -0800

    Correct/add copyright statements

--- java/code/testdata/certificates/build_certs_data.sh
+++ java/code/testdata/certificates/build_certs_data.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 #
-# Copyright 2011 Google Inc.  All Rights Reserved
+# Copyright 2012 Google Inc.
 #
-# Author: swillden@google.com (Shawn Willden)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 #
 # This script was used to generate all of the test keys and certificatees
 # in testdata/certificates.  It is included here primarily for documentation
-# purposes, but could be used to regenerate the keys and certs if requited.
+# purposes, but could be used to regenerate the keys and certs if required.
 
 KEYCZART=../../../../cpp/src/scons-out/dbg-linux/staging/keyczart
 OPENSSL=openssl
