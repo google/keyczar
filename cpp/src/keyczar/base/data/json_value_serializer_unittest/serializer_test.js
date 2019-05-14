0f0727dbc2ee02467f9c927bbf35898aa3be4c45
Shawn Willden <swillden@swillden-macbookpro4.roam.corp.google.com>
2019-05-13 21:28:02 -0500

    Delete all code; Keyczar should not be used.

--- cpp/src/keyczar/base/data/json_value_serializer_unittest/serializer_test.js
+++
@@ -1,7 +0,0 @@
-{
-   "bool": true,
-   "int": 42,
-   "list": [ 1, 2 ],
-   "null": null,
-   "string": "hello"
-}
--- cpp/src/keyczar/base/run_all_unittests.cc
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-#include <gtest/gtest.h>
-
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
